3eea7cc376b8d058d398cb3b956eab954c0f2586
Irvin Capagcuan <irvin@capagcuan.org>
2024-07-08 19:45:43 +0800

    chore: removed unusued code

--- src/server.ts
+++ src/server.ts
@@ -74,9 +74,6 @@ server.all('/*', (req, reply) => {
 const start = async () => {
   try {
     await server.listen({ port: 3000 })
-
-    const address = server.server.address()
-    const port = typeof address === 'string' ? address : address?.port
   } catch (err) {
     server.log.error(err)
     process.exit(1)
